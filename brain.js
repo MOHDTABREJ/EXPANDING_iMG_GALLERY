4d13ca5b44685b4d7a55e56cdcae22829a17ef98
mohd tabrej <1mohdtabrej@gmail.com>
2022-11-05 18:57:00 +0530

    Intial commit

---
+++ brain.js
@@ -0,0 +1,14 @@
+const panels =document.querySelectorAll(".pannel");
+
+panels.forEach((pannel)=>{
+    pannel.addEventListener("click",()=>{
+        removeActiveClasses();
+        pannel.classList.add("active");
+    });
+});
+
+const removeActiveClasses=()=>{
+    panels.forEach((pannel)=>{
+        pannel.classList.remove("active");
+    });
+};
